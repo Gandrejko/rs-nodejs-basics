f37a3e58d9f5614f8ef06204274f518c591d5d72
Andrew <gandrejko2002@gmail.com>
2024-01-29 18:33:38 +0200

    + stream transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { pipeline, Transform } from 'stream';
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    const string = chunk.toString().trim();
+    const reversedString = string.split('').reverse().join('');
+    callback(null, reversedString + '\n');
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  pipeline(
+    process.stdin,
+    reverse,
+    process.stdout,
+    (error) => console.error(error)
+  )
 };
 
 await transform();
